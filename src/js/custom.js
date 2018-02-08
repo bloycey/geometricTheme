4f9de6619dc1e6af30a2e91aee39499670104cee
bloycey <bloyce1990@gmail.com>
2018-02-08 21:28:06 +1000

    Review #1
    Merged css into app.less. Moved JS into custom.js rather than header.

--- src/js/custom.js
+++ src/js/custom.js
@@ -161,6 +161,199 @@ $('.addtocart').click(function(){
 })	
 
 
+/*******/
+//Sticky Header Menu
+
+$('nav').addClass('original').clone().insertAfter('nav').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('background-color', 'rgba(255, 255, 255, 0.85)').css('z-index','500').removeClass('original').hide();
+scrollIntervalID = setInterval(stickIt, 10);
+
+function stickIt() {
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;               
+  if ($(window).scrollTop() >= (orgElementTop)) {
+    // scrolled past the original position; now only show the cloned, sticky element.
+
+    // Cloned element should always have same left position and width as original element.     
+    orgElement = $('.original');
+    coordsOrgElement = orgElement.offset();
+    leftOrgElement = coordsOrgElement.left;  
+    widthOrgElement = orgElement.css('width');
+    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+    $('.original').css('visibility','hidden');
+  } else {
+    // not scrolled past the menu; only show the original menu.
+    $('.cloned').hide();
+    $('.original').css('visibility','visible');
+  }
+}
+      
+var wishlistOpen = false; 
+    $('.primary-fab').click(function(){
+        openWishMenu();
+    })
+
+//Toggle mobile menu arrows
+$('.mob-drop').click(function(){
+    $(this).find('.fa').toggleClass('fa-angle-down fa-angle-up');
+})
+    
+var sideNav = $("#wishlistSideNav");
+
+function mobileWishInit() {
+    $('.wish-sidebar-container').hide();
+    $('#mob-menu').show();
+    $('#menu-icon-open').addClass('secondary-color').css('fontSize', '30px');
+    $('#love-icon-open').removeClass('secondary-color').css('fontSize', '22px');
+}
+
+function desktopWishInit() {
+    $('#mob-menu').hide();
+    $('.wish-sidebar-container').show();
+    $('#love-icon-open').addClass('secondary-color').css('fontSize', '30px');
+    $('#menu-icon-open').removeClass('secondary-color').css('fontSize', '22px');
+}
+
+function openWishMenu() {
+    //Set width of sidenav based on device screensize
+    if (wishlistOpen === false) {
+        $('#background-overlay').addClass('overlay-applied');
+            if ($(window).width() < 767) {
+                $(sideNav).css("left", "25%");
+                mobileWishInit();
+            } else if ($(window).width() < 992){
+                $(sideNav).css("left", "50%");
+                desktopWishInit();
+            } else if ($(window).width() < 1400){
+                $(sideNav).css("left", "70%");
+                desktopWishInit();
+            } else {
+                $(sideNav).css("left", "75%");
+                desktopWishInit();
+            }
+        
+        $('.closed-wishlist-menu').hide();
+        $('.open-wishlist-menu').show();
+
+        document.documentElement.style.overflowY = "hidden";
+        $('#wishlist-items').hide();
+        $.load_ajax_template('_wishlistList');
+        wishlistOpen = true;
+    } 
+    else {
+        //If window is already open then toggle the menu panel.
+        if ($(window).width() < 767) {
+        $('#mob-menu').show();
+        $('.wish-sidebar-container').hide();
+    } else {
+        $('#mob-menu').hide();
+        $('.wish-sidebar-container').show();
+    }
+}} 
+
+
+$('.open-wishlist-menu .wishlist-love-container').click(function(){
+    desktopWishInit();
+})
+
+$('.open-wishlist-menu .wishlist-menu-container').click(function(){
+    mobileWishInit();
+})
+
+$('#background-overlay, .closebtn, .close-fab').click(function(){
+    closeNav();
+})
+     
+function closeNav() {
+    $(sideNav).css('left', '100%');
+    $('#background-overlay').removeClass('overlay-applied');
+        document.documentElement.style.overflowY = "auto";
+    wishlistOpen = false;
+    $('.closed-wishlist-menu').show();
+    $('.open-wishlist-menu').hide();
+};
+
+// On resizing window close the sidemenu.
+
+$(window).on('resize', function(){
+    if (wishlistOpen === true) {
+        closeNav();
+    }
+})
+    
+$(document).ready(function(){
+$('.cart-box').hover(
+    function() {
+        $('.header-cart').addClass('open');
+    }
+)
+
+$('.cart-box').focus(
+    function() {
+        $('.header-cart').addClass('open');
+    }
+)
+
+$('.thumb_cart .left a').focus(
+    function(){
+        $('.right').css("opacity", "1").css("backgroundColor", "white");
+    }
+)
+
+$('.love-box, .mob-drop').blur(
+    function(){
+        $('.right').css("opacity", "0").css("backgroundColor", "transparent");
+        $('.header-cart').removeClass('open');
+    }
+)
+
+$('.cart-box').mouseleave(
+    function() {
+        $('.header-cart').removeClass('open');
+    }
+)
+
+});
+
+ //Wishlist Sidebar
+   
+    $(".primary-fab").click(function(){
+        if(wishlistOpen === false) {
+        load_wishlistList();
+        }
+    })
+
+    $('.sub-category-toggle').click(function(){
+        $(this).toggleClass('fa-angle-down fa-angle-up');
+    })
+
+	function load_wishlist(wishlist){
+        $('#loader').css('display', 'block');
+        $('.wishlist-thumbs').hide();
+         $.load_ajax_template('_wishlist',{mylist:wishlist},{'onLoad':function(){
+             $('#loader').fadeOut('slow');
+             $('.wishlist-thumbs').fadeIn('slow');
+         }});
+         $('#wishlist-items').show();
+    }  
+        
+    function load_wishlistList(){
+        var selected = $('input[name=optradio]:checked', '#wishform').data("id");
+        var selectedVal = $('input[name=optradio]:checked', '#wishform').val();
+        $('#wishlist-items').show();
+        load_wishlist(selectedVal);   
+    }
+
+    function deleteList(url){
+        $("#wishlist-delete").attr("src", url);
+        $("#wishlist-delete").load(function(){
+            $.load_ajax_template('_wishlistList');
+            $('#wishlist-items').hide();
+        })    
+    }
+
+/*******/
+
+
 
 $('.product-image-wrapper').hover(function(){
 	if($(window).width() > 992) {
